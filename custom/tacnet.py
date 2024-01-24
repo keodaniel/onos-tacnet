702f7ff4b57436a9f49b80468394f88059ef3c71
daniel.keo <keodanielc@gmail.com>
2024-01-24 15:50:42 -0800

    bandwidth topo

--- custom/tacnet.py
+++ custom/tacnet.py
@@ -2,10 +2,12 @@ from mininet.net import Mininet
 from mininet.node import Controller
 from mininet.node import Host
 from mininet.topo import Topo
+from mininet.link import TCLink
 
 topos = {
     'base': ( lambda: Base() ), 
-    'DFGW': ( lambda: DFGateway() ) 
+    'DFGW': ( lambda: DFGateway() ),
+    'BW': ( lambda: Bandwidth() )
     }
 
 class Base( Topo ):
@@ -72,4 +74,37 @@ class DFGateway( Topo ):
         self.addLink(s1, s3)
         self.addLink(s2, s3)
         
+class Bandwidth( Topo ):
+    ###TODO: Finalize bandwidth TCLinks
+    "Redundant TCLinks between s1 and s2 of 10Mbps and 5Mbps to emulate . . . "
 
+    def build( self ):
+        "Create custom topo."
+        
+        # Add hosts and switches
+        h1 = self.addHost('h1', ip='10.0.10.1/24', defaultRoute='via 10.0.10.1')
+        h2 = self.addHost('h2', ip='10.0.10.2/24', defaultRoute='via 10.0.10.2')
+        h3 = self.addHost('h3', ip='10.0.20.1/24', defaultRoute='via 10.0.20.1')
+        h4 = self.addHost('h4', ip='10.0.20.2/24', defaultRoute='via 10.0.20.2')
+        h5 = self.addHost('h5', ip='10.0.30.1/24', defaultRoute='via 10.0.30.1')
+        h6 = self.addHost('h6', ip='10.0.30.2/24', defaultRoute='via 10.0.30.2')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        # Add links
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        self.addLink(h5, s3)
+        self.addLink(h6, s3)
+        
+        # Interunit Links
+        self.addLink(s1, s2, cls=TCLink, bw=10) # 10 MBps
+        self.addLink(s1, s2, cls=TCLink, bw=5) # 5 MBps
+
+        # Backhaul Links
+        self.addLink(s1, s3)
+        self.addLink(s2, s3)
+  
