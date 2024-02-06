b249b04f45d3469770b3c1ed6b9117a32bceae48
daniel.keo <keodanielc@gmail.com>
2024-02-05 19:34:52 -0800

    Correct host variable names

--- custom/tacnet.py
+++ custom/tacnet.py
@@ -87,8 +87,8 @@ class Bandwidth( Topo ):
         
         # Add hosts and switches
         h1 = self.addHost('h1', ip='10.0.10.1/24', defaultRoute='via 10.0.10.1')
-        h3 = self.addHost('h2', ip='10.0.20.1/24', defaultRoute='via 10.0.20.1')
-        h5 = self.addHost('h3', ip='10.0.30.1/24', defaultRoute='via 10.0.30.1')
+        h2 = self.addHost('h2', ip='10.0.20.1/24', defaultRoute='via 10.0.20.1')
+        h3 = self.addHost('h3', ip='10.0.30.1/24', defaultRoute='via 10.0.30.1')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
