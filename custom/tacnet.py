07aa3e8dec3183a5004cb5598d3ec5f2f45eba06
daniel.keo <keodanielc@gmail.com>
2024-04-04 12:12:27 -0700

    Adjust TC Topo for 6 Hosts

--- custom/tacnet.py
+++ custom/tacnet.py
@@ -78,26 +78,32 @@ class DFGateway( Topo ):
         self.addLink(s2, s3)
 
 class TC( Topo ):
-    """DFGW with 50MBps bandwidth between units"""
+    """DFGW with 10MBps bandwidth between units"""
 
     def build( self ):
         "Create custom topo."
         
         # Add hosts and switches
         h1 = self.addHost('h1', ip='10.0.10.1/24', defaultRoute='via 10.0.10.1')
-        h2 = self.addHost('h2', ip='10.0.20.1/24', defaultRoute='via 10.0.20.1')
-        h3 = self.addHost('h3', ip='10.0.30.1/24', defaultRoute='via 10.0.30.1')
+        h2 = self.addHost('h2', ip='10.0.10.2/24', defaultRoute='via 10.0.10.2')
+        h3 = self.addHost('h3', ip='10.0.20.1/24', defaultRoute='via 10.0.20.1')
+        h4 = self.addHost('h4', ip='10.0.20.2/24', defaultRoute='via 10.0.20.2')
+        h5 = self.addHost('h5', ip='10.0.30.1/24', defaultRoute='via 10.0.30.1')
+        h6 = self.addHost('h6', ip='10.0.30.2/24', defaultRoute='via 10.0.30.2')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
 
         # Add links
         self.addLink(h1, s1)
-        self.addLink(h2, s2)
-        self.addLink(h3, s3)
-
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        self.addLink(h5, s3)
+        self.addLink(h6, s3)
+        
         # Interunit Links
-        self.addLink(s1, s2, cls=TCLink, bw=10)
+        self.addLink(s1, s2, cls=TCLink, bw=10,)
 
         # Backhaul Links
         self.addLink(s1, s3, cls=TCLink, bw=10)
